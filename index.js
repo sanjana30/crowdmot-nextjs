915becf9ee1591fd8bda20c4255da2347b2b1298
Sanjana Tripathi <sanjanatripathi@sanjanas-mbp.lan>
2020-04-30 12:05:19 -0500

    modified index.js in root director

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { createServer } = require('http')
-const next = require('next')
-const dev = (process.env.NODE_ENV || '').startsWith('dev')
-const app = next({ dev })
-const handle = app.getRequestHandler()
-app.prepare().then(() => {
-  createServer(handle).listen(3000, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+// const { createServer } = require('http')
+// const next = require('next')
+// const dev = (process.env.NODE_ENV || '').startsWith('dev')
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+// app.prepare().then(() => {
+//   createServer(handle).listen(3000, err => {
+//     if (err) throw err
+//     console.log('> Ready on http://localhost:3000')
+//   })
+// })
